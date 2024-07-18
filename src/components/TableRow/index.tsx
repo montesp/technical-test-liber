366e40dc1e5bbf3202e432a0e7ce028e31bd42b8
Pavel Montes <pavelmontes22@gmail.com>
2024-07-18 14:15:47 -0600

    feat: State and genre cell stylized

--- src/components/TableRow/index.tsx
+++ src/components/TableRow/index.tsx
@@ -13,10 +13,16 @@ export const TableRow = ( { user }: Props ) => {
         { user.email }
       </td>
       <td className="table__cell table__cell--body">
-        { user.gender}
+        <div className={`table__cell--status ${user.gender === 'female' ? 'table__cell--women':'table__cell--men'}`}>
+          <p>
+            { user.gender === 'female' ? 'Mujer':'Hombre'}
+          </p>
+        </div>
       </td>
       <td className="table__cell table__cell--body">
-        { user.status}
+        <div className={`table__cell--status ${user.status === 'active' ? 'table__cell--active':'table__cell--inactive'}`}>
+          { user.status === 'active' ? 'Activo':'Inactivo'}
+        </div>
       </td>
       <td className="table__cell table__cell--body">
         <button className="button">D</button>
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -25,6 +25,9 @@ export const HomePage = () => {
       <h1 className='home__title'>
         Lista de Usuarios
       </h1>
+      <div className="home__buttons">
+        <button className="button button--create">Crear usuario</button>
+      </div>
       <UserList/>
     </main>
   );
